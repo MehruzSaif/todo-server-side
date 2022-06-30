129c57c34ae670f73caa0a967295d80781048b45
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-07-01 03:07:47 +0600

    node cors set up

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello Todofy!')
+})
+
+app.listen(port, () => {
+    console.log(`Todofy app listening on port ${port}`)
+})
